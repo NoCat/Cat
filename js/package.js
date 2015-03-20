eaf97a91bba0e79e92443e901a54005123903b7f
Hoody123 <Hoody_gs@qq.com>
2015-03-20 12:13:38 +0800

    个人页面数据填充
    user.js waterfallPush函数完善,以便日后扩展功能
    package.js 数据填充

--- js/package.js
+++ js/package.js
@@ -1,1 +1,34 @@
-﻿
+﻿/// <reference path="Widget/package.js" />
+/// <reference path="main.js" />
+/// <reference path="Widget/Image.js" />
+/// <reference path="Widget/package.js" />
+/// <reference path="Widget/package-info.js" />
+
+$(function () {
+    var frame = MPWidget.Frame.New();
+    var packageInfo = MPWidget.PackageInfo.New(MPData.package);
+    frame.Body.append(packageInfo);
+    $("body").append(frame);
+
+    switch (MPData.sub1) {
+
+        case "follower":
+            waterfallPush(packageInfo, MPWidget.User, null, "id");
+            break;
+
+        default:
+            waterfallPush(packageInfo, MPWidget.Package, null, "id");
+            break;
+    }
+
+    function waterfallPush(packageinfo, type, typeDetail, returnField) {
+        var max = packageinfo.waterfall.Push(MPData.datas, type, typeDetail, returnField);
+        packageinfo.waterfall.onBottom = function () {
+            packageinfo.waterfall.BeginUpdate();
+            $.getJSON("", { ajax: true, max: max }, function (data) {
+                max = packageinfo.waterfall.Push(data, type, typeDetail, returnField);
+                packageinfo.waterfall.EndUpdate();
+            });
+        }
+    }
+})
